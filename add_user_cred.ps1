9667ab8b46a853e6d88c7f7779e91e14967ec7b5
Skyrider <79767107+skyrid3r698@users.noreply.github.com>
2024-11-29 12:11:14 +0100

    better logging

--- add_user_cred.ps1
+++ add_user_cred.ps1
@@ -1,3 +1,7 @@
+# Starting Transcript
+$null = Start-Transcript -Append $env:TEMP\add_user_cred.txt
+Write-Host $(Get-Date)"[INFO]Starte Logging nach $env:TEMP\add_user_cred-Log.txt"
+
 $domainname = "lv.asb-mv.de"
 $servername = "files22.lv.asb-mv.de"
 $NuGetInstalled = $null
@@ -6,32 +10,37 @@ $CredentialManagerInstalled = $null
 $CredentialManagerInstalled = Get-InstalledModule -Name CredentialManager > $null
 
 if ($NuGetInstalled -eq $null) {
-Write-Host "NuGet ist nicht instaliert. Installiere..."
+Write-Host $(Get-Date)"[INFO]NuGet ist nicht instaliert. Installiere..."
+Try {
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser
 }
+catch {
+Write-Host $(Get-Date)"[ERROR]Es ist ein Fehler während der Installation von NuGet aufgetreten. Prüfe das Log unter" -ForegroundColor Red
+}
+}
 else {
-Write-Host "NuGet ist installiert. Installation wird übersprungen.."
+Write-Host $(Get-Date)"[INFO]NuGet ist installiert. Installation wird Übersprungen.."
 }
 
 if ($CredentialManagerInstalled -eq $null) {
-Write-Host "CredentialManager ist nicht installiert. Installiere.."
+Write-Host $(Get-Date)"[INFO]CredentialManager ist nicht installiert. Installiere.."
 Try {
 Install-Module CredentialManager -Force -SkipPublisherCheck -Scope CurrentUser -Confirm:$False
 }
 #maybe nuget needs a restart of powershell
 catch {
-Write-Host "CredentailManager Install failed. Trying again.."
+Write-Host $(Get-Date)"[WARN]CredentailManager Install failed. Trying again.." -ForegroundColor Yellow
 start powershell $PSCommandPath
 exit
 }
 Import-Module CredentialManager 
 }
 else {
-Write-Host "CredentialManager ist installiert. Installation wird übersprungen.."
+Write-Host $(Get-Date)"[INFO]CredentialManager ist installiert. Installation wird Ã¼bersprungen.."
 }
 
 $PW = $null
-Write-Host "Damit die Netzlaufwerke der ASB Umgebung angebunden werden können, ist eine Authentifizierung mit Ihrem ASB Nutzer erforderlich."
+Write-Host $(Get-Date)"[INFO]Damit die Netzlaufwerke der ASB Umgebung angebunden werden können, ist eine Authentifizierung mit Ihrem ASB Nutzer erforderlich."
 
 $PW = Get-Credential -Message "Damit die Netzlaufwerke der ASB Umgebung angebunden werden können, ist eine Authentifizierung mit Ihrem ASB Nutzer erforderlich." -User "$domainname\"
 if ($PW -eq $null) {
